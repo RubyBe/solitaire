0a5e800c88982405a3708c2fe4ce884e2f62c8be
RubyBe <benavente@outlook.com>
2017-01-07 10:27:14 -0800

    through with services

--- src/Solitaire/wwwroot/_references.js
+++ src/Solitaire/wwwroot/_references.js
@@ -1,4 +1,7 @@
 ﻿/// <autosync enabled="true" />
+/// <reference path="js/app.js" />
+/// <reference path="js/dealer.js" />
+/// <reference path="js/maincontroller.min.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
